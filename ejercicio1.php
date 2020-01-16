3358ec0811599731d8ed26969b07cf8ff3f59165
Pedro Morales <pedromorales@grupojuinsa.es>
2020-01-16 13:28:37 +0100

    creo php con conexion a la BD y muestro datos de la tabla alumnos

---
+++ ejercicio1.php
@@ -0,0 +1,25 @@
+<?php
+
+$usuario = "root";
+$contrasena = "132456";
+$servidor = "localhost";
+$basededatos = "alumnos";
+
+$conexion = mysqli_connect( $servidor, $usuario, $contrasena ) or die ("No se ha podido conectar al servidor de Base de datos");
+
+$db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
+
+$consulta = "SELECT * FROM alumnos";
+
+$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+
+echo "<ul>";
+
+while ($columna = mysqli_fetch_array( $resultado ))
+{
+    echo "<li>" . $columna['nombre'] . " - " . $columna['edad'] . " años</li>";
+}
+
+echo "</ul>";
+
+?>
